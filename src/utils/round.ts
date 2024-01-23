22f111a530d2d43cfc546f780eb4b47639f6f075
Ivo Dolenc <ivodolenc@gmail.com>
2024-01-23 11:40:04 +0100

    refactor: add `round` util

---
+++ src/utils/round.ts
@@ -0,0 +1,15 @@
+/**
+ * Rounds a number with a specified precision.
+ *
+ * @example
+ *
+ * ```ts
+ * import { round } from 'animer'
+ *
+ * round(32.997633923673) // => 33
+ * ```
+ */
+export function round(number: number, precision: number = 2) {
+  precision = 10 ** precision
+  return Math.round(number * precision) / precision
+}
