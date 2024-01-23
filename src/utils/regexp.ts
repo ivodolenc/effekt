01c484296e0db596776c1778914838f63d8f13ec
Ivo Dolenc <ivodolenc@gmail.com>
2024-01-23 11:38:59 +0100

    refactor: add `regexp` utils

---
+++ src/utils/regexp.ts
@@ -0,0 +1,32 @@
+const composeRegex = (...regxs: RegExp[]) =>
+  new RegExp(regxs.map((r) => r.source).join('|'))
+
+export const rgxBrackets = /\((.*?)\)/
+
+export const rgxDigits = /(-?[0-9.]+)/g
+
+export const rgxDigitsOnly = /^\d+$/
+
+export const rgxColor = /^(#|rgb|hsl)/
+
+export const rgxIsColor = /(color|background)/i
+
+export const rgxUnits =
+  /(px|pt|pc|in|cm|mm|em|rem|%|ex|ch|fr|vw|vh|vmin|vmax|deg|rad|turn)/
+
+export const rgxPxTransform = /^(x|y|z|translate|perspective)/
+
+export const rgxPxOther =
+  /(width|height|margin|padding|inset|top|right|bottom|left)/i
+
+export const rgxDegTransform = /^(rotate|skew)/
+
+export const rgxUnitlessTransform = /^scale/
+
+export const rgxIsTransform = composeRegex(
+  rgxPxTransform,
+  rgxDegTransform,
+  rgxUnitlessTransform,
+)
+
+export const rgxPxAll = composeRegex(rgxPxTransform, rgxPxOther)
