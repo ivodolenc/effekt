6a50165a033537cb81e61603a5b75706da2f9d84
Ivo Dolenc <ivodolenc@gmail.com>
2024-01-23 10:13:33 +0100

    refactor: add `clamp` util

---
+++ src/utils/clamp.ts
@@ -0,0 +1,14 @@
+/**
+ * Clamps a value within a range of `min` and `max` values.
+ *
+ * @example
+ *
+ * ```ts
+ * import { clamp } from 'animer'
+ *
+ * clamp(0, 100, 60) // => 60
+ * ```
+ */
+export function clamp(min: number, max: number, value: number): number {
+  return Math.min(Math.max(value, min), max)
+}
