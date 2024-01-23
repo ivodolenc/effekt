c230ffbed5bf0e41735e54be71e2247bdc79d407
Ivo Dolenc <ivodolenc@gmail.com>
2024-01-23 10:25:54 +0100

    refactor: add `pipe` util

---
+++ src/utils/pipe.ts
@@ -0,0 +1,17 @@
+/**
+ * Combines multiple functions into a single pipeline.
+ *
+ * @example
+ *
+ * ```ts
+ * import { pipe } from 'animer'
+ *
+ * const add3 = (v: number) => v + 3
+ * const sub2 = (v: number) => v - 2
+ *
+ * pipe(add3, sub2)(6) // => 7
+ * ```
+ */
+export function pipe<T>(...functions: ((arg: T) => T)[]): (v: T) => T {
+  return (v: T) => functions.reduce((a, b) => b(a), v)
+}
