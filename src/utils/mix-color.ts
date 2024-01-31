3e44dcfb8961eae033239e32303cd03588284f49
Ivo Dolenc <ivodolenc@gmail.com>
2024-01-31 14:52:35 +0100

    refactor: add `mix-color` util

---
+++ src/utils/mix-color.ts
@@ -0,0 +1,15 @@
+/**
+ * Linearly interpolates color between the `from` and `to` values, based on `progress`.
+ *
+ * @example
+ *
+ * ```ts
+ * import { mixColor } from 'animer'
+ *
+ * mixColor(0, 100, 0.3) // => 54.772
+ * ```
+ */
+export function mixColor(from: number, to: number, progress: number): number {
+  const fromExpo = from * from
+  return Math.sqrt(Math.max(0, progress * (to * to - fromExpo) + fromExpo))
+}
