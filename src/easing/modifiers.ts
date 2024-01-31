1197323970e9254bcd1d98431a9076c0c8ef4e0d
Ivo Dolenc <ivodolenc@gmail.com>
2024-01-31 15:02:12 +0100

    refactor: add `modifiers` easing utils

---
+++ src/easing/modifiers.ts
@@ -0,0 +1,51 @@
+import type { EasingModifier } from '@/types/easing'
+
+/**
+ * Creates a `reverse` easing modifier.
+ *
+ * Turns `ease-in` into `ease-out` effect.
+ *
+ * @example
+ *
+ * ```ts
+ * import { easingOut } from 'animer/easing'
+ *
+ * const easeOut = easingOut(easeIn)
+ * ```
+ */
+export const easingOut: EasingModifier = (easing) => (progress) =>
+  1 - easing(1 - progress)
+
+/**
+ * Creates a `mirror` easing modifier.
+ *
+ * Turns `ease-in` into `ease-in-out` effect.
+ *
+ * @example
+ *
+ * ```ts
+ * import { easingInOut } from 'animer/easing'
+ *
+ * const easeInOut = easingInOut(easeIn)
+ * ```
+ */
+export const easingInOut: EasingModifier = (easing) => (progress) =>
+  progress < 0.5 ? easing(progress * 2) / 2 : 1 - easing(progress * -2 + 2) / 2
+
+/**
+ * Creates a `reverse-mirror` easing modifier.
+ *
+ * Turns `ease-in` into `ease-out-in` effect.
+ *
+ * @example
+ *
+ * ```ts
+ * import { easingOutIn } from 'animer/easing'
+ *
+ * const easeOutIn = easingOutIn(easeIn)
+ * ```
+ */
+export const easingOutIn: EasingModifier = (easing) => (progress) =>
+  progress < 0.5
+    ? (1 - easing(1 - progress * 2)) / 2
+    : (easing(progress * 2 - 1) + 1) / 2
