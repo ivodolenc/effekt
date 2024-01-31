77eb25690c86c08ebdddd3344d9c44828acced91
Ivo Dolenc <ivodolenc@gmail.com>
2024-01-31 16:18:56 +0100

    refactor: add `steps` easing

---
+++ src/easing/steps.ts
@@ -0,0 +1,24 @@
+import { clamp } from '@/utils'
+import type { Easing } from '@/types'
+
+/**
+ * Creates a `steps` easing effect.
+ *
+ * @example
+ *
+ * ```ts
+ * import { steps } from 'animer/easing'
+ *
+ * animate('.el', {
+ *   ease: steps(3),
+ * })
+ * ```
+ */
+export function steps(
+  intervals: number,
+  position: 'start' | 'end' = 'end',
+): Easing {
+  const round = position === 'end' ? Math.floor : Math.ceil
+
+  return (p) => clamp(0, 1, round(p * intervals) / intervals)
+}
