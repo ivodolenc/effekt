f3f6a549a50064629f606401e984db3f9d3f1060
Ivo Dolenc <ivodolenc@gmail.com>
2024-02-08 14:43:04 +0100

    feat: add new `setStyle` dom util

---
+++ src/dom/set-style.ts
@@ -0,0 +1,29 @@
+import { isUndefined } from '@/utils/is'
+
+/**
+ * Sets a single CSS style to the element.
+ *
+ * The `property` and `value` parameters should follow the built-in syntax of the `CSSStyleDeclaration` interface.
+ *
+ * @example
+ *
+ * ```ts
+ * import { setStyle } from 'effekt'
+ *
+ * setStyle(el, 'background', 'rgba(255, 255, 255, 1)')
+ * ```
+ */
+export function setStyle(
+  el: HTMLElement | SVGElement,
+  property: string,
+  value: string,
+): string {
+  const p = property as any
+
+  if (!isUndefined(el.style[p])) {
+    return (el.style[p] = value)
+  }
+
+  const id = el.id || el.classList[0] || el.tagName
+  throw new TypeError(`Unsupported '${property}' property in '${id}' target.`)
+}
