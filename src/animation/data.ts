acd9c1df915176a7b271455c49633a33e9daec24
Ivo Dolenc <ivodolenc@gmail.com>
2024-03-05 13:15:35 +0100

    feat: add animation default data

---
+++ src/animation/data.ts
@@ -0,0 +1,26 @@
+import type { AnimationData } from '@/types/animation'
+
+export const defaultData: Omit<
+  AnimationData,
+  | 'autoplay'
+  | 'direction'
+  | 'playRate'
+  | 'duration'
+  | 'delayStart'
+  | 'delayEnd'
+  | 'repeat'
+> = {
+  delta: 0,
+  timestamp: 0,
+  playState: 'idle',
+  promiseState: 'pending',
+  time: 0,
+  initTime: 0,
+  startTime: 0,
+  pauseTime: null,
+  totalDuration: 600,
+  progress: 0,
+  totalProgress: 1,
+  reverseMode: false,
+  reverseTime: 0,
+}
