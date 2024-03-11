22d7d622a9a3992d65dddbbd11c9aaf50ea7a7c6
Ivo Dolenc <ivodolenc@gmail.com>
2024-03-11 17:00:36 +0100

    types: update stagger options types

--- src/types/utils/stagger.ts
+++ src/types/utils/stagger.ts
@@ -1,6 +1,11 @@
+import { Easing } from '../shared'
+
 export type StaggerOrigin = 'first' | 'center' | 'last' | number
 
 export type StaggerOptions = {
   delay?: number
   from?: StaggerOrigin
+  ease?: Easing
+  grid?: [number, number]
+  axis?: 'x' | 'y'
 }
