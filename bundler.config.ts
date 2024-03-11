19fb3c660c8472ee0ca9e8f0713f5ff89bd6a57d
Ivo Dolenc <ivodolenc@gmail.com>
2024-03-11 17:23:52 +0100

    chore: update bundler main config

--- bundler.config.ts
+++ bundler.config.ts
@@ -1,21 +1,56 @@
 import { defineConfig } from '@hypernym/bundler'
 import { version, homepage } from './package.json'
 
-const banner = `/*! effekt v${version} | ${homepage} | MIT License | Ivo Dolenc (c) 2024 */\n`
+const banner = `/*! effekt ${version} | ${homepage} | MIT License | Ivo Dolenc (c) 2024 */\n`
 
 export default defineConfig({
   alias: true,
   entries: [
     {
-      // effekt esm
+      // main esm
       input: './src/index.ts',
       output: './dist/index.mjs',
       banner,
     },
     {
-      // effekt types
+      // main esm minified
+      input: './src/index.ts',
+      output: './dist/index.min.mjs',
+      plugins: { esbuild: { minify: true } },
+      banner,
+    },
+    {
+      // main iife minified
+      input: './src/index.ts',
+      output: './dist/index.iife.min.js',
+      plugins: { esbuild: { minify: true } },
+      format: 'iife',
+      name: 'Effekt',
+      banner,
+    },
+    {
+      // main umd minified
+      input: './src/index.ts',
+      output: './dist/index.umd.min.js',
+      plugins: { esbuild: { minify: true } },
+      format: 'umd',
+      name: 'Effekt',
+      banner,
+    },
+    {
+      // main types
       types: './src/types/index.ts',
       output: './dist/types/index.d.ts',
+    },
+    {
+      // easing esm
+      input: './src/easing/index.ts',
+      output: './dist/easing/index.mjs',
+    },
+    {
+      // easing types
+      types: './src/types/easing/index.ts',
+      output: './dist/types/easing/index.d.ts',
     },
   ],
 })
