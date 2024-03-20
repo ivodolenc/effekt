71ef8c24458fdf954fc7736161fd728b64637abc
Ivo Dolenc <ivodolenc@gmail.com>
2024-03-20 17:12:04 +0100

    feat: add driver delay util

---
+++ src/engine/set-delay.ts
@@ -0,0 +1,17 @@
+import { secToMs } from '@/utils'
+import { isFunction, isUndefined } from '@/utils/is'
+import type { DelayFunction } from '@/types'
+
+export function setDelay(
+  delay?: number | DelayFunction,
+  index?: number,
+  total?: number,
+): number {
+  if (delay && !isUndefined(index) && !isUndefined(total)) {
+    return isFunction(delay) ? secToMs(delay(index, total)) : secToMs(delay)
+  }
+  if (delay && isUndefined(index) && isUndefined(total)) {
+    return isFunction(delay) ? secToMs(delay(0, 1)) : secToMs(delay)
+  }
+  return 0
+}
