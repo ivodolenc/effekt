d2cec998c998123a64caf74b808a738adcee76a8
Ivo Dolenc <ivodolenc@gmail.com>
2024-04-01 18:08:39 +0200

    types: update get-elements types

---
+++ src/types/utils/get-elements.ts
@@ -0,0 +1,1 @@
+export type ParsedElements = (HTMLElement | SVGElement)[]
--- src/types/utils/index.ts
+++ src/types/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './mix'
 export * from './interpolate'
+export * from './get-elements'
 export * from './stagger'
 
 export * from '@/utils'
