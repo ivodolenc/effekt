274cbb04ea86267fc8561bf645a6a06bc7891c6e
Ivo Dolenc <ivodolenc@gmail.com>
2024-04-07 16:37:33 +0200

    types: update keyframe generator types

--- src/types/animation/keyframes/generator.ts
+++ src/types/animation/keyframes/generator.ts
@@ -3,7 +3,4 @@ import type { KeyframeOptions } from './options'
 export interface KeyframesGenerator {
   options: KeyframeOptions[]
   force3d: boolean
-  hasTransform: boolean
-  hasFilter: boolean
-  hasTranslateZ: boolean
 }
