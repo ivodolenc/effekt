54bb37d706dfd9fa6d7eaf35f25ce963b3fb0732
Ivo Dolenc <ivodolenc@gmail.com>
2024-04-16 15:49:29 +0200

    types: update interaction types

--- src/types/interaction/scroll/index.ts
+++ src/types/interaction/scroll/index.ts
@@ -1,6 +1,16 @@
 export type OnScroll = (progress: number) => void
 
 export interface ScrollOptions {
+  /**
+   * Specifies the element whose scroll position drives the progress of the timeline.
+   *
+   * @default document.documentElement
+   */
   source?: HTMLElement
+  /**
+   * Specifies the scroll axis that drives the progress of the timeline.
+   *
+   * @default 'y'
+   */
   axis?: 'x' | 'y'
 }
--- src/types/interaction/utils/timeline.ts
+++ src/types/interaction/utils/timeline.ts
@@ -1,5 +1,5 @@
 export declare class ProgressTimeline<T> {
   constructor(options: T)
-  currentTime: { value: number } | null
   cancel: VoidFunction
+  progress: number
 }
