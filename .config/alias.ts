d94ede43bf99ff7117d9daea7586f15b55359009
Ivo Dolenc <ivodolenc@gmail.com>
2024-08-29 20:41:12 +0200

    chore: update vscode config

--- .config/alias.ts
+++
@@ -1,9 +0,0 @@
-import { cwd } from 'node:process'
-import { resolve } from 'node:path'
-
-const dir = `${resolve(cwd(), './src')}/`
-
-export const alias = [
-  { find: /^@\//, replacement: dir },
-  { find: /^~\//, replacement: dir },
-]
--- .config/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import { jsConfig, tsConfig, ignoresConfig } from '@hypernym/eslint-config'
-
-/** @type {import("eslint").Linter.Config[]} */
-export default [jsConfig, tsConfig, ignoresConfig]
