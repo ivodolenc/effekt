c0662e3914c24c2ead1fbd71e25e3cc7723bc168
Ivo Dolenc <ivodolenc@gmail.com>
2024-08-29 19:11:31 +0200

    chore: update dev configs

--- .config/eslint.config.js
+++ .config/eslint.config.js
@@ -1,17 +1,4 @@
 import { jsConfig, tsConfig, ignoresConfig } from '@hypernym/eslint-config'
 
-export default [
-  jsConfig,
-  tsConfig,
-  ignoresConfig,
-  {
-    files: tsConfig.files,
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { ignoreRestSiblings: true },
-      ],
-    },
-  },
-]
+/** @type {import("eslint").Linter.Config[]} */
+export default [jsConfig, tsConfig, ignoresConfig]
--- .config/vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite'
-import { alias } from './alias'
-
-export default defineConfig({
-  resolve: { alias },
-})
