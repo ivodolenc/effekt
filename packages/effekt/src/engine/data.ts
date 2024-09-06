6578ed0328e1e797a59d08c4340ecff9132d06e7
Ivo Dolenc <ivodolenc@gmail.com>
2024-09-06 14:48:05 +0200

    refactor: improve driver data defaults

--- packages/effekt/src/engine/data.ts
+++ packages/effekt/src/engine/data.ts
@@ -1,11 +1,34 @@
-import { secToMs } from '@/utils'
-import { setDelay } from './utils/set-delay'
-import { setRepeat } from './utils/set-repeat'
-import type { DriverData, DriverDataOptions, AnimationTarget } from '@/types'
+import { isFunction, secToMs } from '@/utils'
+import type {
+  DriverData,
+  DriverDataOptions,
+  AnimationTarget,
+  DelayFunction,
+} from '@/types'
+
+function setDelay(
+  delay: number | DelayFunction | undefined,
+  index: number,
+  total: number,
+): number {
+  if (delay) {
+    return isFunction(delay) ? secToMs(delay(index, total)) : secToMs(delay)
+  }
+  return 0
+}
+
+function setRepeat(repeat?: number): number {
+  return repeat ? (repeat === Infinity ? 1000 : repeat + 1) : 1
+}
 
 export function createDriverData(options: DriverDataOptions = {}): DriverData {
-  const { el } = options
-
+  const el =
+    options.el ||
+    ({
+      value: {},
+      index: 0,
+      total: 1,
+    } as AnimationTarget)
   const data: WeakMap<AnimationTarget['value'], DriverData> = new WeakMap()
 
   data.set(el!.value, {
@@ -26,8 +49,8 @@ export function createDriverData(options: DriverDataOptions = {}): DriverData {
     direction: options.direction || 'normal',
     playRate: options.playRate || 1,
     duration: secToMs(options.duration || 0.6),
-    delay: setDelay(options.delay, el?.index, el?.total),
-    endDelay: setDelay(options.endDelay, el?.index, el?.total),
+    delay: setDelay(options.delay, el.index, el.total),
+    endDelay: setDelay(options.endDelay, el.index, el.total),
     repeat: setRepeat(options.repeat),
   })
 
--- packages/effekt/src/engine/utils/set-repeat.ts
+++
@@ -1,3 +0,0 @@
-export function setRepeat(repeat?: number): number {
-  return repeat ? (repeat === Infinity ? 1000 : repeat + 1) : 1
-}
