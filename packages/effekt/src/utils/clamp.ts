a053078007c79a27cb083f995cd717777d38aeb3
Ivo Dolenc <ivodolenc@gmail.com>
2025-01-21 17:55:06 +0100

    refactor: update clamp util

--- packages/effekt/src/utils/clamp.ts
+++ packages/effekt/src/utils/clamp.ts
@@ -1,12 +1,21 @@
 /**
- * Clamps a value within a range of `min` and `max` values.
+ * Clamps a value between a specified minimum and maximum.
+ *
+ * Ensures that the given `value` will not be smaller than `min` or greater than `max`.
+ *
+ * It returns the `value` if it is within the range, otherwise, it returns the nearest boundary (`min` or `max`).
  *
  * @example
  *
  * ```ts
- * import { clamp } from 'effekt'
+ * import { clamp } from 'effekt/utils'
  *
- * clamp(0, 100, 60) // => 60
+ * // Clamps the value 10 between 1 and 5, returns 5
+ * clamp(1, 5, 10)
+ * // Clamps the value -3 between 0 and 5, returns 0
+ * clamp(0, 5, -3)
+ * // No change, as 3 is between 1 and 5, returns 3
+ * clamp(1, 5, 3)
  * ```
  */
 export function clamp(min: number, max: number, value: number): number {
