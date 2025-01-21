a193722d2fcc6952deb6501c090d69744661e418
Ivo Dolenc <ivodolenc@gmail.com>
2025-01-21 18:07:15 +0100

    refactor: update round util

--- packages/effekt/src/utils/round.ts
+++ packages/effekt/src/utils/round.ts
@@ -1,15 +1,18 @@
 /**
- * Rounds a number with a specified precision.
+ * Rounds a number to a specified number of decimal places.
  *
  * @example
  *
  * ```ts
- * import { round } from 'effekt'
+ * import { round } from 'effekt/utils'
  *
- * round(32.997633923673) // => 33
+ * round(3.14159, 2) // Returns 3.14
+ * round(3.14159, 3) // Returns 3.142
+ * round(5.5555, 2) // Returns 5.56
+ * round(1.987654, 4) // Returns 1.9877
  * ```
  */
-export function round(number: number, precision: number = 2) {
+export function round(number: number, precision: number = 2): number {
   precision = 10 ** precision
   return Math.round(number * precision) / precision
 }
