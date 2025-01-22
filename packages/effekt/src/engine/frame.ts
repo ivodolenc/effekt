597ae7c91e27be6c54f8dace6661f3457ff764e8
Ivo Dolenc <ivodolenc@gmail.com>
2025-01-22 15:24:11 +0100

    refactor: clean up engine dir

--- packages/effekt/src/engine/frame.ts
+++
@@ -1,8 +0,0 @@
-import { isBrowser, noop } from '@/utils'
-import { createRenderBatcher } from './batcher'
-
-export const {
-  schedule: frame,
-  cancel: cancelFrame,
-  state: frameData,
-} = createRenderBatcher(isBrowser ? requestAnimationFrame : noop, true)
