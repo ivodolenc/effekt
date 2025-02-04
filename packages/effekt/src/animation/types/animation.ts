dab6bda29e54c9249df8c5c8ff52340f7a4392ba
Ivo Dolenc <ivodolenc@gmail.com>
2025-02-04 11:27:20 +0100

    types: update types

--- packages/effekt/src/animation/types/animation.ts
+++ packages/effekt/src/animation/types/animation.ts
@@ -9,10 +9,15 @@ export interface Animation {
   cancel(): void
   completed: AnimationPromise
   get startTime(): number
+  set startTime(t: number)
   get time(): number
+  set time(t: number)
   get playRate(): number
+  set playRate(r: number)
   get effect(): globalThis.AnimationEffect | null
+  set effect(e: globalThis.AnimationEffect | null)
   get timeline(): globalThis.AnimationTimeline | null
+  set timeline(t: globalThis.AnimationTimeline | null)
   get playState(): Readonly<globalThis.AnimationPlayState>
   get progress(): Readonly<number>
   get isCompleted(): Readonly<boolean>
